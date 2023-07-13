fc382f9fb7dea05dff44190f85d7ac917f3ad72b
khalidh7 <103854400+khalidh7@users.noreply.github.com>
2023-07-13 15:17:07 -0700

    login page

--- brainforce-ui/src/Components/App/App.jsx
+++ brainforce-ui/src/Components/App/App.jsx
@@ -1,23 +1,22 @@
 import { Navbar } from "../Navbar/Navbar";
-import { Home } from '../Home/Home';
-import { Login } from '../Login/Login';
-import { Register } from '../Register/Register.jsx';
-import './App.css';
+import { Home } from "../Home/Home";
+import { Login } from "../Login/Login";
+import { Register } from "../Register/Register.jsx";
+import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
-    <Navbar />
+      <Navbar />
 
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/register" element={ <Register />} />
-      <Route path="/login" element={<Login />} />
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </BrowserRouter>
-
-  )
+  );
 }
 
 export default App;
