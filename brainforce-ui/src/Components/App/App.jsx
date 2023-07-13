0465b7d14feb5cc8abb8a80d879965ddab9b4f7e
oyindamola <oyindamola.akanbi@oyindam-ltmujtm.internal.salesforce.com>
2023-07-13 11:46:13 -0700

    registration

--- brainforce-ui/src/Components/App/App.jsx
+++ brainforce-ui/src/Components/App/App.jsx
@@ -1,10 +1,24 @@
-
 import { Navbar } from "../Navbar/Navbar";
+import { Home } from '../Home/Home';
+import { Login } from '../Login/Login';
+import { Navbar } from "../Navbar/Navbar";
+import { Home } from '../Home/Home.jsx';
+import { Login } from '../Login/Login.jsx';
+import { Register } from '../Register/Register.jsx';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
+    <BrowserRouter>
     <Navbar />
+
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/register" element={ <Register />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+    </BrowserRouter>
 
   )
 }
---
+++ brainforce-ui/src/Components/Register/Register.jsx
@@ -0,0 +1,18 @@
+import "./Register.css"
+
+export function Register() {
+    return (
+        <div className="registration-page-container">
+            <div className= "registration-container">
+                <section>
+                    <h1>Brainforce</h1>
+                </section>
+                <section>
+                    Register
+                </section>
+
+            </div>
+            
+        </div>
+    )
+}
