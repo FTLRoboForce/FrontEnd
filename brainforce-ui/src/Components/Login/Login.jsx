4860286d72aa974f30094211fae037ce2c598d73
khalidh7 <103854400+khalidh7@users.noreply.github.com>
2023-07-17 15:00:40 -0700

    login functionality done

--- brainforce-ui/src/Components/Login/Login.jsx
+++ brainforce-ui/src/Components/Login/Login.jsx
@@ -2,36 +2,37 @@ import React from "react";
 import "./Login.css";
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import Api from "../../utilities/api";
 
-export function Login() {
+export function Login({ setToken, setUserGlobal }) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [user, setUser] = useState({ email: email, password: password });
   const [name, setName] = useState();
   const navigate = useNavigate();
 
-  //   function handleOnSubmit(event) {
-  //     event.preventDefault();
-  //     Api.login(user).then((response) => {
-  //       localStorage.setItem("jwt", response.token);
-  //       setToken(response.token);
-  //     });
-  //     Api.user({ token: localStorage.getItem("jwt") }).then((response) => {
-  //       setUserGlobal(response);
-  //       setName(response.firstname + " " + response.lastname);
-  //     });
-  //     navigate("/activity");
-  //   }
+  function handleOnSubmit(event) {
+    event.preventDefault();
+    Api.login(user).then((response) => {
+      localStorage.setItem("jwt", response.token);
+      setToken(response.token);
+    });
+    Api.user({ token: localStorage.getItem("jwt") }).then((response) => {
+      setUserGlobal(response);
+      setName(response.firstname + " " + response.lastname);
+    });
+    // navigate("/activity");
+  }
 
-  //   function handleOnChangeLoginEmail(email) {
-  //     setEmail(email);
-  //     setUser({ email: email, password: password });
-  //   }
+  function handleOnChangeLoginEmail(email) {
+    setEmail(email);
+    setUser({ email: email, password: password });
+  }
 
-  //   function handleOnChangeLoginPassword(password) {
-  //     setPassword(password);
-  //     setUser({ email: email, password: password });
-  //   }
+  function handleOnChangeLoginPassword(password) {
+    setPassword(password);
+    setUser({ email: email, password: password });
+  }
   return (
     <div className="login-container">
       <h1>Login</h1>
@@ -43,9 +44,9 @@ export function Login() {
           <input
             type="email"
             id="email"
-            // onChange={(event) => {
-            //   handleOnChangeLoginEmail(event.target.value);
-            // }}
+            onChange={(event) => {
+              handleOnChangeLoginEmail(event.target.value);
+            }}
             placeholder="Enter your email"
             className="form-input-login"
           />
@@ -57,9 +58,9 @@ export function Login() {
           <input
             type="password"
             id="password"
-            // onChange={(event) => {
-            //   handleOnChangeLoginPassword(event.target.value);
-            // }}
+            onChange={(event) => {
+              handleOnChangeLoginPassword(event.target.value);
+            }}
             placeholder="Enter your password"
             className="form-input-login"
           />
@@ -67,14 +68,14 @@ export function Login() {
         <button
           type="submit"
           className="submit-button"
-          //   onClick={(event) => {
-          //     handleOnSubmit(event);
-          //   }}
+          onClick={(event) => {
+            handleOnSubmit(event);
+          }}
         >
           Login
         </button>
       </form>
-      {/* {name?.length > 0 ? <h1>Hello {name}</h1> : null} */}
+      {name?.length > 0 ? <h1>Hello {name}</h1> : null}
     </div>
   );
 }
