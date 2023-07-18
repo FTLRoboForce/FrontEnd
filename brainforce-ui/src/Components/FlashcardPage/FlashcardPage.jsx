76a418708bb157bb72f8e68073af701e1085b6f6
khalidh7 <103854400+khalidh7@users.noreply.github.com>
2023-07-18 13:23:59 -0700

    flip thru flashcards

--- brainforce-ui/src/Components/FlashcardPage/FlashcardPage.jsx
+++ brainforce-ui/src/Components/FlashcardPage/FlashcardPage.jsx
@@ -1,11 +1,67 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Flashcard from "../Flashcard/Flashcard";
 import "./FlashcardPage.css";
+import { useState } from "react";
 
 export default function FlashcardPage() {
+  const [counter, setCounter] = useState(0);
+  const [flashcards, setFlashcards] = useState([
+    {
+      front: "Front 1",
+      back: "Back 1"
+    },
+    {
+      front: "Front 2",
+      back: "Back 2"
+    },
+    {
+      front: "Front 3",
+      back: "Back 3"
+    }
+  ]);
+
+  // make sure counter doesn't go more than the number of flashcards
+
+  const handlePreviousClick = () => {
+    if (counter > 0) {
+      setCounter(counter - 1);
+    } else {
+      setCounter(flashcards.length - 1);
+    }
+  };
+
+  const handleNextClick = () => {
+    if (counter < flashcards.length - 1) {
+      setCounter(counter + 1);
+    } else {
+      setCounter(0);
+    }
+  };
+
   return (
     <div className="flashcard-container">
-      <Flashcard />
+      <div className="flash-content">
+        <Flashcard flashcard={flashcards[counter]} />
+      </div>
+
+      <div className="flash-buttons">
+        <button
+          className="flashcard-button"
+          onClick={() => {
+            handlePreviousClick();
+          }}
+        >
+          Previous
+        </button>
+        <button
+          className="flashcard-button"
+          onClick={() => {
+            handleNextClick();
+          }}
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 }
