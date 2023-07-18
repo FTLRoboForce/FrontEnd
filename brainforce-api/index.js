b82706cdaf2b4ea3f40a552742da8d091b1944b2
Wissam Hassani <whassani@whassan-ltm46qf.internal.salesforce.com>
2023-07-18 15:59:29 -0700

    api call working

--- brainforce-api/index.js
+++ brainforce-api/index.js
@@ -1,28 +1,33 @@
 const express = require("express")
+const cors = require('cors')
 require("dotenv").config()
 const {Configuration, OpenAIApi} = require("openai")
+const bodyParser = require('body-parser')
 const app = express()
+app.use(cors())
+app.use(bodyParser.json())
 app.use(express.json())
 
 const config = new Configuration({
     apiKey : process.env.OPEN_AI_KEY,
-    basePath: 'https://api.openai.com/v1/chat/completions'
-
 })
+
 
 const openAi = new OpenAIApi(config)
 
+
 app.post("/flashcard", async (req,res) => {
+    const { number, difficultyLevel, subject, optionalSection } = req.body
     try{
-        const { number, difficultyLevel, subject, optionalSection } = req.body
         const response = await openAi.createCompletion({
-            model : "gpt-3.5-turbo-0613",
-            prompt: `Create ${number} ${difficultyLevel} flashcards about ${subject} specifically ${optionalSection}.`, max_tokens: 100, temperature: 0,top_p: 1.0,frequency_penalty: 0.0,presence_penalty: 0.0, stop: ["\n"]
+            model : "text-davinci-003",
+            prompt:  `Create ${number} unique ${difficultyLevel} flashcards about ${subject} specifically ${optionalSection}.The questions are meant for academic uses so don't include too broad questions. Return in json format,get rid of new lines,and have question and answer in different key-value pairs`, max_tokens: 50, temperature:1.5,top_p: 1.0,frequency_penalty: 0.0,presence_penalty: 0.0, stop: ["{}"]
         })
         return res.status(200).json({
             success : true,
             data : response.data.choices[0].text
-        })
+        }
+        )
     }
     catch(error){
         return res.status(400).json({
