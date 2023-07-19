3dbac78f1d3d249b235f1e180a209e1bcc92e167
Wissam Hassani <whassani@whassan-ltm46qf.internal.salesforce.com>
2023-07-19 10:02:20 -0700

    better prompt

--- brainforce-api/index.js
+++ brainforce-api/index.js
@@ -21,7 +21,10 @@ app.post("/flashcard", async (req,res) => {
     try{
         const response = await openAi.createCompletion({
             model : "text-davinci-003",
-            prompt:  `Create ${number} unique ${difficultyLevel} flashcards about ${subject} specifically ${optionalSection}.The questions are meant for academic uses so don't include too broad questions. Return in json format,get rid of new lines,and have question and answer in different key-value pairs`, max_tokens: 50, temperature:1.5,top_p: 1.0,frequency_penalty: 0.0,presence_penalty: 0.0, stop: ["{}"]
+            prompt:  `Create ${number} unique ${difficultyLevel} flashcard(s) about ${subject} specifically ${optionalSection}.
+            The question(s) are meant for academic uses so don't include too broad question(s). Response should be returned as an array of json objects where the response would look like:
+            [{"question" : "What is the general formula for alkane?", "answer": "CnH2n+2"}]`
+            , max_tokens: 100, temperature:.2,top_p: 1.0,frequency_penalty: 2.0,presence_penalty: 2.0, stop: ["[]"]
         })
         return res.status(200).json({
             success : true,
