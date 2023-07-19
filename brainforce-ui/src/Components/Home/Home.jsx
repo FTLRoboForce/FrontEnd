059bdde01d9bd8198b2b448203b7ab9ff8cce1cf
oyindamola <oyindamola.akanbi@oyindam-ltmujtm.internal.salesforce.com>
2023-07-18 21:44:44 -0700

    Landing page css updated// Make course subject logo uodated

--- brainforce-ui/src/Components/Home/Home.jsx
+++ brainforce-ui/src/Components/Home/Home.jsx
@@ -1,59 +1,45 @@
 import React from "react";
 import logo from "../../images/brainForce-logo.png";
 import einstein from "../../images/einstein.png";
-import hero  from "../../images/bf.png";
+import hero from "../../images/bf.png";
 import "./Home.css";
 import lightbulb from "../../images/lightbulb.png";
 
 export function Home() {
   return (
-    <>
-    <div className="home">
-      <header>
-        <section className="hero-container">
-          <section className="logo-container-home">
-            <h3>Learn, Quiz, Conquer</h3>
-            </section>
-            <div className="hero-image-wrapper">
-            <section className="hero-image-container">
-            <img src={hero} alt = "hero image" />
-              </section>
-          <section className= "hero-title-container ">
-            <h3>
-              BrainForce!
-            </h3>
-          </section>
-            </div>
- 
+    <div className="homePage-container">
+      <div className="lightbulb-container-left">
+        <img src={lightbulb} alt="Lightbulb" />
+      </div>
+
+      <div className="hero-home-container">
+        <section className="hero-title">
+          <h3>Learn, Quiz, Conquer</h3>
         </section>
 
-      </header>
-      <main>
-        <section className="heroo">
-          <div className="hero-content">
-          <a href="/register" className="cta-button">Get Started</a>
-         
-            <p>Unlock your potential with BrainForce, the ultimate learning platform.</p>
-        
-          </div>
+        <section className="hero-image">
+          <img src={hero} alt="hero" />
         </section>
 
-      </main>
-      <footer>
-        <p>&copy; 2023 BrainForce. All rights reserved.</p>
-      </footer>
-    </div>
+        <section className="hero-title-container">
+          <h2>BrainForce!</h2>
+        </section>
 
-    <div id="lightbulb-left">
-      <img src= {lightbulb}>
-      </img>
-    </div>
-    <div id="lightbulb-right">
-      <img src= {lightbulb}>
-      </img>
-    </div>
-    
-    </>
+        <div className="homePage-title">
+          <a href="/register" className="cta-button">
+            Get Started
+          </a>
+          <p>Unlock your potential with BrainForce, the ultimate learning platform.</p>
+        </div>
 
+        <div className="footer">
+          <p>&copy; 2023 BrainForce. All rights reserved.</p>
+        </div>
+      </div>
+
+      <div className="lightbulb-container-right">
+        <img src={lightbulb} alt="Lightbulb" />
+      </div>
+    </div>
   );
 }
