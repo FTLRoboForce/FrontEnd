63c30c2b9b21b537f835cba69f1a2085089c29a1
OJ Akanbi <95331700+ojakanbi@users.noreply.github.com>
2023-07-23 17:47:17 -0700

    registration page UI updated

--- brainforce-ui/src/Components/Register/Register.jsx
+++ brainforce-ui/src/Components/Register/Register.jsx
@@ -1,32 +1,48 @@
-import React, { useState } from 'react';
-import { useToggle, upperFirst } from '@mantine/hooks';
-import { TextInput, PasswordInput, Text, Paper, Group, Button, Divider, Checkbox, Anchor, Stack, Image } from '@mantine/core';
-import ParticleBackground from '../../ParticleBackground/ParticleBackground';
+import React, { useState } from "react";
+import { useToggle, upperFirst } from "@mantine/hooks";
+import {
+  TextInput,
+  PasswordInput,
+  Text,
+  Paper,
+  Group,
+  Button,
+  Divider,
+  Checkbox,
+  Anchor,
+  Stack,
+  Image,
+} from "@mantine/core";
+import ParticleBackground from "../../ParticleBackground/ParticleBackground";
 import Api from "../../utilities/api";
-import  google  from "../../images/google.svg"
+import google from "../../images/google.svg";
 
-import './Register.css'; 
+import "./Register.css";
 
 const initialUserState = {
-  email: '',
-  firstname: '',
-  lastname: '',
-  password: '',
-  confirm: '',
+  email: "",
+  firstname: "",
+  username: "",
+  lastname: "",
+  password: "",
+  confirm: "",
   points: 0,
 };
 
 export function Register() {
   const [user, setUser] = useState(initialUserState);
-  const [errortext, setErrortext] = useState('');
+  const [errortext, setErrortext] = useState("");
 
   function handleOnSubmit(event) {
     event.preventDefault();
-    
-    if (verifyPassword(user.password, user.confirm) && verifyEmail(user.email)) {
+
+    if (
+      verifyPassword(user.password, user.confirm) &&
+      verifyEmail(user.email)
+    ) {
       Api.register(user);
       setUser(initialUserState);
-      setErrortext(''); // Clear any previous error messages
+      setErrortext(""); // Clear any previous error messages
     }
   }
 
@@ -34,7 +50,7 @@ export function Register() {
     const { name, value } = event.target;
     setUser((prevUser) => ({
       ...prevUser,
-      [name]: value
+      [name]: value,
     }));
   }
 
@@ -66,101 +82,120 @@ export function Register() {
 
   return (
     <>
-   
-    <ParticleBackground/>
-    <div className="container"> 
-      <div className="registration-page-container">
-       
-        <Paper radius="md" p="xl" withBorder>
-        <Text  align="center"
-        color="#004D85" 
-        sx={(theme) => ({ fontFamily: `Greycliff CF, ${theme.fontFamily}`, fontWeight: 900,  fontSize: 26, } ) }
-       >
-          Welcome to Brainforce!
-        </Text>
-          <Text size="lg" weight={500}>
-            Register with
-          </Text>
+      <ParticleBackground />
+      <div className="container">
+        <div className="registration-page-container">
+          <Paper radius="md" p="xl" withBorder>
+            <Text
+              align="center"
+              color="#004D85"
+              sx={(theme) => ({
+                fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+                fontWeight: 900,
+                fontSize: 26,
+              })}
+            >
+              Welcome to Brainforce!
+            </Text>
+            <Text size="lg" weight={500}>
+              Register with
+            </Text>
 
-          <Group grow mb="md" mt="md">
-          <button id='google-btn'>  <Image src = {google} width={20}/>
-          </button>
-      
-       
-          </Group>
+            <Group grow mb="md" mt="md">
+              <button id="google-btn">
+                {" "}
+                <Image src={google} width={20} />
+              </button>
+            </Group>
 
-          <Divider label="Or continue with email" labelPosition="center" my="lg" />
+            <Divider
+              label="Or continue with email"
+              labelPosition="center"
+              my="lg"
+            />
 
-          <form onSubmit={handleOnSubmit}>
-            <Stack>
-              <TextInput
-                required
-                label="First Name"
-                placeholder="Enter your first name"
-                value={user.firstname}
-                onChange={handleValueChange}
-                name="firstname"
-              />
+            <form onSubmit={handleOnSubmit}>
+              <Stack>
+                <TextInput
+                  required
+                  label="First Name"
+                  placeholder="Enter your first name"
+                  value={user.firstname}
+                  onChange={handleValueChange}
+                  name="firstname"
+                />
 
-              <TextInput
-                required
-                label="Last Name"
-                placeholder="Enter your last name"
-                value={user.lastname}
-                onChange={handleValueChange}
-                name="lastname"
-              />
+                <TextInput
+                  required
+                  label="Last Name"
+                  placeholder="Enter your last name"
+                  value={user.lastname}
+                  onChange={handleValueChange}
+                  name="lastname"
+                />
 
-              <TextInput
-                required
-                label="Email"
-                placeholder="Enter your email"
-                value={user.email}
-                onChange={handleValueChange}
-                name="email"
-                error={errortext && errortext.includes("Email") && errortext}
-              />
+                <TextInput
+                  required
+                  label="Email"
+                  placeholder="Enter your email"
+                  value={user.email}
+                  onChange={handleValueChange}
+                  name="email"
+                  error={errortext && errortext.includes("Email") && errortext}
+                />
 
-              <PasswordInput
-                required
-                label="Password"
-                placeholder="Enter your password"
-                value={user.password}
-                onChange={handleValueChange}
-                name="password"
-              />
+                <TextInput
+                  required
+                  label="Username"
+                  placeholder="Enter your username"
+                  value={user.username}
+                  onChange={handleValueChange}
+                  name="username"
+                  error={errortext && errortext.includes("username") && errortext}
+                />
 
-              <PasswordInput
-                required
-                label="Confirm Password"
-                placeholder="Confirm your password"
-                value={user.confirm}
-                onChange={handleValueChange}
-                name="confirm"
-                error={errortext && errortext.includes("Passwords") && errortext}
-              />
+                <PasswordInput
+                  required
+                  label="Password"
+                  placeholder="Enter your password"
+                  value={user.password}
+                  onChange={handleValueChange}
+                  name="password"
+                />
 
-              <Checkbox
-                required
-                label="I accept terms and conditions"
-               
-                // onChange={(event) => setUser((prevUser) => ({ ...prevUser, terms: event.currentTarget.checked }))}
-              />
+                <PasswordInput
+                  required
+                  label="Confirm Password"
+                  placeholder="Confirm your password"
+                  value={user.confirm}
+                  onChange={handleValueChange}
+                  name="confirm"
+                  error={
+                    errortext && errortext.includes("Passwords") && errortext
+                  }
+                />
 
-              <Button type="submit" radius="xl" className='submit-button'>
-                Register
-              </Button>
+                <Checkbox
+                  required
+                  label="I accept terms and conditions"
 
-              {errortext && !errortext.includes("Passwords") && !errortext.includes("Email") && (
-                <p className="error-text">{errortext}</p>
-              )}
-            </Stack>
-          </form>
+                  // onChange={(event) => setUser((prevUser) => ({ ...prevUser, terms: event.currentTarget.checked }))}
+                />
 
-          
-        </Paper>
+                <Button type="submit" radius="xl" className="submit-button">
+                  Register
+                </Button>
+
+                {errortext &&
+                  !errortext.includes("Passwords") &&
+                  !errortext.includes("Email") && (
+                    <p className="error-text">{errortext}</p>
+                  )}
+              </Stack>
+            </form>
+          </Paper>
+        </div>
       </div>
-    </div>
     </>
   );
 }
