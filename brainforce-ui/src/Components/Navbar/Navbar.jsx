aedf437824f802fb91670182cac13d8cf24403d3
oyindamola <oakanbi13@gmail.com>
2023-07-25 23:13:11 -0700

    nav bar UI

--- brainforce-ui/src/Components/Navbar/Navbar.jsx
+++ brainforce-ui/src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,9 @@
 import React, { useEffect, useState } from "react";
 import logo from "../../images/bf.png";
 import { Link } from "react-router-dom";
+
+import "./Navbar.css";
 import {
-  createStyles,
-  Group,
-  Text,
-  rem,
   Image,
   UnstyledButton,
   Avatar,
@@ -15,224 +13,99 @@ import {
 import {
   IconLogout,
   IconHeart,
-  IconStar,
-  IconMessage,
-  IconSettings,
-  IconPlayerPause,
-  IconTrash,
-  IconSwitchHorizontal,
   IconChevronDown
 } from "@tabler/icons-react";
 
 import { useDisclosure } from "@mantine/hooks";
-import { cx } from "@mantine/core";
-import ParticleBackground from "../../ParticleBackground/ParticleBackground";
 
-const useStyles = createStyles((theme) => ({
-  navContainer: {
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
-    padding: theme.spacing.xs,
-    background: `linear-gradient(to right, ${theme.colors.blue[2]}, ${theme.colors.blue[9]})`,
-    borderBottom: `solid 1px ${theme.colors.blue[2]}`,
-  },
-  logoContainer: {
-    display: "flex",
-    alignItems: "center",
-    cursor: "pointer"
-  },
-  logoImage: {
-    width: rem(40),
-    height: rem(40),
-    marginRight: rem(10)
-  },
-  logoText: {
-    fontWeight: 900,
-    color: "var(--darkblue)",
-    fontSize: rem(22)
-  },
-  navLinksContainer: {
-    display: "flex",
-    alignItems: "center"
-  },
-
-  navLink: {
-    marginLeft: rem(20),
-    color: theme.white,
-    textDecoration: "none",
-    fontWeight: 500,
-    fontSize: rem(16),
-    cursor: "pointer",
-    "&:hover": {
-      borderBottom: `2px solid ${theme.white}`
-    }
-  },
-  userActivityContainer: {
-    display: "flex",
-    alignItems: "center"
-  },
-  userActivityButton: {
-    marginLeft: rem(20),
-    color: theme.white,
-    backgroundColor: theme.colors.blue[7],
-    borderRadius: theme.radius.md,
-    padding: `${theme.spacing.xs} ${theme.spacing.md}`,
-    border: "none",
-    fontWeight: 500,
-    fontSize: rem(16),
-    cursor: "pointer",
-    "&:hover": {
-      backgroundColor: theme.colors.blue[6]
-    }
-  },
-  user: {
-    cursor: "pointer",
-    color: theme.colors.blue[6],
-    fontWeight: 500,
-    fontSize: rem(14),
-    lineHeight: rem(20),
-    display: "flex",
-    alignItems: "center",
-    background: "transparent",
-    border: "none",
-    textDecoration: "none"
-  },
-  userActive: {
-    color: theme.colors.blue[9]
-  }
-}));
-
-export function Navbar({ token, setToken, userGlobal }) {
-  const { classes, theme, cx } = useStyles();
+export function Navbar({ token, setToken }) {
   const [opened, { toggle }] = useDisclosure(false);
   const [userMenuOpened, setUserMenuOpened] = useState(false);
 
-  //get username in localstorage
   const username = localStorage.getItem("username");
-
 
   function logOut() {
     localStorage.removeItem("jwt");
-    localStorage.removeItem("username"); // Remove the stored username when the user logs out
+    localStorage.removeItem("username");
     setToken(null);
     setUserMenuOpened(false);
     window.location = "/";
-
   }
 
-
- 
   return (
-    <>
-   
-
-    <header className={classes.navContainer}>
-      <div  onClick={() => (window.location = "/")} className={classes.logoContainer}>
-        <div className={classes.logoImage}>
-          <Image
-            src={logo}
-            alt="BrainForce Logo"
-           
-          />
+    <header className="navContainer">
+      <div onClick={() => (window.location = "/")} className="logoContainer">
+        <div className="logoImage">
+          <Image src={logo} alt="BrainForce Logo" />
         </div>
-        <Text className={classes.logoText}>BrainForce</Text>
+        <div className="logoText">BrainForce</div>
       </div>
       {token ? (
-        <div className={classes.navLinksContainer}>
-          <Link to="/makecourse" className={classes.navLink}>
+        <div className="navLinksContainer">
+          <Link to="/makecourse" className="navLink">
             Make Course
           </Link>
-          <Link to="/flashcard" className={classes.navLink}>
+          <Link to="/flashcard" className="navLink">
             Previous Flashcard
           </Link>
-          <Link to="/quiz" className={classes.navLink}>
+          <Link to="/quiz" className="navLink">
             Previous Quizzes
           </Link>
-          <Link to="/leaderboard" className={classes.navLink}>
+          <Link to="/leaderboard" className="navLink">
             Leaderboard
           </Link>
 
           <Menu>
             <Menu.Target>
-              <UnstyledButton
-                className={cx(classes.user, {
-                  [classes.userActive]: userMenuOpened
-                })}
-              >
-                <Group
-                  spacing={7}
-                  style={{
-                    background: `linear-gradient(to left, var(--lightgrey), var(--lightblue))`,
-                    borderRadius: rem(5),
-                    boxShadow: `0px 2px 6px rgba(0, 0, 0, 0.15)`,
-                    marginBottom: rem(10),
-                    marginLeft: rem(5)
-                  }}
-                >
-                  <Avatar src={logo} alt={username} radius="xl" size={20} />
-                  <Text
-                    color={"#004D85"}
-                    weight={500}
-                    size="sm"
-                    sx={{ lineHeight: 1 }}
-                    mr={3}
-                  >
-                    {username}
-                  </Text>
-                  <IconChevronDown size={rem(12)} stroke={1.5} />
-                </Group>
+              <UnstyledButton className="userActivityButton">
+                <div className="userActivityContainer">
+                  <Avatar
+                    src={logo}
+                    alt={username}
+                    radius="xl"
+                    size={20}
+                 
+                  />
+                  <div className="menu-item">{username}</div>
+                  <IconChevronDown size={16} />
+                </div>
               </UnstyledButton>
             </Menu.Target>
             <Menu.Dropdown>
-              <Menu.Item
-                icon={
-                  <IconHeart
-                    size="0.9rem"
-                    color={theme.colors.red[6]}
-                    stroke={1.5}
-                  />
-                }
-              >
+              <Menu.Item icon={<IconHeart size="0.9rem" />}>
                 Points
               </Menu.Item>
 
               <Menu.Divider />
 
-              <Menu.Item
-                onClick={logOut}
-                icon={<IconLogout size="0.9rem" stroke={1.5} />}
-              >
+              <Menu.Item onClick={logOut} icon={<IconLogout size="0.9rem" />}>
                 Logout
               </Menu.Item>
             </Menu.Dropdown>
           </Menu>
         </div>
       ) : (
-        <div className={classes.navLinksContainer}>
-          <div className={classes.userActivityContainer}>
-            <button
-              onClick={() => (window.location = "/creators")}
-              className={classes.userActivityButton}
-            >
-              Creators
-            </button>
-            <button
-              onClick={() => (window.location = "/login")}
-              className={classes.userActivityButton}
-            >
-              Login
-            </button>
-            <button
-              onClick={() => (window.location = "/register")}
-              className={classes.userActivityButton}
-            >
-              Sign Up
-            </button>
-          </div>
+        <div className="userActivityContainer">
+          <button
+            onClick={() => (window.location = "/creators")}
+            className="userActivityButton"
+          >
+            Creators
+          </button>
+          <button
+            onClick={() => (window.location = "/login")}
+            className="userActivityButton"
+          >
+            Login
+          </button>
+          <button
+            onClick={() => (window.location = "/register")}
+            className="userActivityButton"
+          >
+            Sign Up
+          </button>
         </div>
       )}
     </header>
-    </>
   );
 }
