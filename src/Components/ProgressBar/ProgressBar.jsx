ad5dcbea2184b26a8cbfc5f898674d40f6063914
oyindamola <oakanbi13@gmail.com>
2023-07-27 19:01:17 -0700

    progresss Bar done

--- src/Components/ProgressBar/ProgressBar.jsx
+++ src/Components/ProgressBar/ProgressBar.jsx
@@ -1,30 +1,34 @@
 import React, { useState, useEffect } from 'react';
 import './ProgressBar.css';
 
+const steps = ['learn', 'quiz', 'conquer'];
+
 export default function ProgressBar({ progressBar, setProgressBar }) {
   const [activeStep, setActiveStep] = useState('learn');
 
   useEffect(() => {
-    // Update the active step based on the prop 'ProgressBar'
-    // 'learn', 'quiz', 'conquer'
     setActiveStep(progressBar);
   }, [progressBar, setActiveStep]);
+
+  const calculateProgressWidth = () => {
+    const stepIndex = steps.indexOf(activeStep);
+    const widthPercentage = (stepIndex + 1) * (100 / steps.length);
+    return `${widthPercentage}%`;
+  };
 
   return (
     <div className='progressBar-container'>
       <div className='progressBar'>
-        <form id='msform'>
-          <ul id='progressbar'>
-            <li className={activeStep === 'learn' ? 'active' : ''}>Learn</li>
-            <li className={activeStep === 'quiz' ? 'active' : ''}>Quiz</li>
-            <li className={activeStep === 'conquer' ? 'active' : ''}>Conquer</li>
-          </ul>
-          <div className="progress-line-container">
-            <div className={`progress-line ${activeStep}`} />
-          </div>
-        </form>
-
-       
+        <ul id='progressbar'>
+          {steps.map((step) => (
+            <li key={step} className={activeStep === step ? 'active' : ''}>
+              {step.charAt(0).toUpperCase() + step.slice(1)}
+            </li>
+          ))}
+        </ul>
+        <div className="progress-line-container">
+          <div className="progress-line" style={{ width: calculateProgressWidth() }} />
+        </div>
       </div>
     </div>
   );
