a1e986d3b236e06cd10462852d6a7c9c6c4a0835
oyindamola <oakanbi13@gmail.com>
2023-08-01 21:59:24 -0700

    leaderboard styling updated

--- src/Components/Leaderboard/Leaderboard.jsx
+++ src/Components/Leaderboard/Leaderboard.jsx
@@ -1,42 +1,39 @@
 import React, { useEffect, useState, useRef } from "react";
-import "./Leaderboard.css";
+import "./Leaderboard.css"; // Updated CSS file with "2" in classnames
 import Api from "../../utilities/api";
-import ProgressBar from "../ProgressBar/ProgressBar";
-import { Image, UnstyledButton, Avatar, Menu } from "@mantine/core";
+import ProgressBar from "../ProgressBar/ProgressBar"; // Updated ProgressBar component with "2" in classname
+import { Avatar } from "@mantine/core";
 
 const LeaderboardRow = ({ data, index, userGlobal }) => {
   const dateString = data.created;
   const date = new Date(dateString);
 
   const formattedDate = date.toLocaleDateString("en-US", {
-    month: "2-digit", // MM
-    day: "2-digit", // DD
-    year: "numeric" // YYYY
+    month: "2-digit",
+    day: "2-digit",
+    year: "numeric",
   });
 
   return (
-    <div
-      className={`leaderboard-row ${index % 2 === 0 ? "even-row" : "odd-row"}`}
-    >
-     <div style={{ display: "flex", alignItems: "center"  ,marginLeft:"30px"}}>
+    <tr className={`leaderboard-row2 ${index % 2 === 0 ? "even-row2" : "odd-row2"}`}>
+      <td className="user-info2">
         <Avatar src={data?.photo} radius="xl" size={30} />
-        <p style={{ marginLeft: "10px" }}>{data?.username}</p>
-      </div>
-      <p style= {{marginLeft:"70px"}}>{data.points}</p>
-      <p style= {{marginLeft:"30px"}}>{data.totalquiz}</p>
-      <p style= {{marginLeft:"30px"}}> {formattedDate}</p>
-    </div>
+        <span className="username2">{data?.username}</span>
+      </td>
+      <td className="points2">{data.points}</td>
+      <td className="total-quizzes2">{data.totalquiz}</td>
+      <td className="date-joined2">{formattedDate}</td>
+    </tr>
   );
 };
 
 export default function Leaderboard({
   userGlobal,
   progressBar,
-  setProgressBar
+  setProgressBar,
 }) {
   const [leaderboardData, setLeaderboardData] = useState([]);
   const leaderboardContainerRef = useRef(null);
-  console.log(userGlobal?.photo)
 
   useEffect(() => {
     try {
@@ -49,9 +46,8 @@ export default function Leaderboard({
   }, []);
 
   useEffect(() => {
-    // Add a cool animation when the user gets on the page
     if (leaderboardContainerRef.current) {
-      leaderboardContainerRef.current.classList.add("appear-animation");
+      leaderboardContainerRef.current.classList.add("appear-animation2");
     }
   }, []);
 
@@ -60,26 +56,34 @@ export default function Leaderboard({
   return (
     <>
       {userGlobal ? (
-        <div className="leaderboardPage-container">
+        <div className="leaderboardPage-container2">
           <ProgressBar progressBar={progressBar} />
-          <div className="leaderboard-container" ref={leaderboardContainerRef}>
+          <div className="leaderboard-container2" ref={leaderboardContainerRef}>
             <h2>Leaderboard</h2>
-            <div className="leaderboard-titles">
-              <p className="leaderboard-title">Username</p>
-              <p className="leaderboard-title">Points</p>
-              <p className="leaderboard-title">Total Quizzes Taken</p>
-              <p className="leaderboard-title">Date Joined</p>
-            
-            </div>
-            <div className="leaderboard-content">
-              {leaderboardData.map((data, index) => (
-                <LeaderboardRow key={index} data={data} index={index} userGlobal={userGlobal} />
-              ))}
-            </div>
+            <table className="leaderboard-table2">
+              <thead>
+                <tr className="leaderboard-titles2">
+                  <th className="leaderboard-title2">Username</th>
+                  <th className="leaderboard-title2">Points</th>
+                  <th className="leaderboard-title2">Total Quizzes Taken</th>
+                  <th className="leaderboard-title2">Date Joined</th>
+                </tr>
+              </thead>
+              <tbody className="leaderboard-content2">
+                {leaderboardData.map((data, index) => (
+                  <LeaderboardRow
+                    key={index}
+                    data={data}
+                    index={index}
+                    userGlobal={userGlobal}
+                  />
+                ))}
+              </tbody>
+            </table>
           </div>
         </div>
       ) : (
-        <div className="flashcardPage-container">Please Log In</div>
+        <div className="flashcardPage-container2">Please Log In</div>
       )}
     </>
   );
