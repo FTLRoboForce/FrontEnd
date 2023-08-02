3b81354f8ba8cd91a071de659410c855314edcff
oyindamola <oakanbi13@gmail.com>
2023-08-02 00:19:06 -0700

    creator page being worked on, need to update each creator... button stylings and hover has been taking over

---
+++ src/Components/Creators/Creator.jsx
@@ -0,0 +1,484 @@
+import React, { useState } from "react";
+import "./Creator.css";
+import sac from "./sac.png";
+import kahlid from "./kahlid.png";
+import pennstate from "./pennstate.png";
+import oj from "./oj.png";
+import fiu from "./fiu.png";
+import hassani from "./hassani.png";
+import ParticleBackground from "../../ParticleBackground/ParticleBackground";
+
+const Card = () => {
+  const [activeSection, setActiveSection] = useState("#about");
+
+  const handleButtonClick = (targetSection) => {
+    setActiveSection(targetSection);
+  };
+
+  return (
+    <>
+
+    <ParticleBackground />
+    
+    <div className="card-container">
+
+   
+   
+    <div className="card" data-state={activeSection}>
+      <div className="card-header">
+        <div
+          className="card-cover"
+          style={{
+            backgroundImage:
+              "url('https://play-lh.googleusercontent.com/tP1lwh_oLWW3XuX9qBKdKKNIBUNfQ_chTM_1GxuTxhnNb_fXYtMzB4gKBoVJx7RviSk')",
+          }}
+        ></div>
+        <img
+          className="card-avatar"
+          src="https://media.licdn.com/dms/image/D4E03AQFeh3JOjHO7eQ/profile-displayphoto-shrink_400_400/0/1689808542545?e=1696464000&v=beta&t=sa9cAbUKEHR7mcqLCRGLI6Lp6u86R2VD2rIqEDpv9vM"
+          alt="avatar"
+        />
+        <h1 className="card-fullname">Oyindamola J Akanbi</h1>
+        <h2 className="card-jobtitle">Software Engineer</h2>
+      </div>
+      <div className="card-main">
+        <div
+          className={`card-section ${
+            activeSection === "#about" ? "is-active" : ""
+          }`}
+          id="about"
+        >
+          <div className="card-content">
+            <div className="card-subtitle">ABOUT</div>
+            <p className="card-desc">
+              I am a Software Engineer with a passion for building scalable
+            </p>
+          </div>
+          <div className="card-social">
+            {/* Social media icons here */}
+          </div>
+        </div>
+        <div
+          className={`card-section ${
+            activeSection === "#experience" ? "is-active" : ""
+          }`}
+          id="experience"
+        >
+          {/* Experience section content here */}
+          <div className="card-content">
+            <div className="card-subtitle">WORK EXPERIENCE</div>
+            <div className="card-timeline">
+              <div className="card-item" data-year="2024">
+                <div className="card-item-title">
+                Returning Software Engineer Intern <span> @ Salesforce</span>
+                </div>
+                <div className="card-item-desc">
+                 
+                </div>
+              </div>
+              <div className="card-item" data-year="2023">
+                <div className="card-item-title">
+                  Future Tech Launchpad   <span> @ SalesForce</span>
+                </div>
+                <div className="card-item-desc">
+                Full Stack Web Development PERN stack!
+                </div>
+              </div>
+            
+             
+            </div>
+          </div>
+        </div>
+        <div
+          className={`card-section ${
+            activeSection === "#contact" ? "is-active" : ""
+          }`}
+          id="contact"
+        >
+          {/* Contact section content here */}
+          <div className="card-content">
+            <div className="card-subtitle">CONTACT</div>
+            <div className="card-contact-wrapper">
+              <div className="card-contact">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0118 0z" />
+                  <circle cx="12" cy="10" r="3" />
+                </svg>
+                415 Mission St California City, CA 94105
+              </div>
+              <div className="card-contact">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <path d="M12 19v-6M12 8c0-2.2 3.6-4 8-4s8 1.8 8 4-3.6 4-8 4-8-1.8-8-4z" />
+                </svg>
+                oakanbi13@gmail.com
+              </div>
+              <div className="card-contact">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <path d="M22 2l-10 9L2 2" />
+                  <path d="M22 2l-1 6 7 7-9 9 1 6 10-9 9 9 1-6-7-7 9-9z" />
+                </svg>
+                (267) 709-5092
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="card-buttons">
+          <button
+            onClick={() => handleButtonClick("#about")}
+            data-section="#about"
+            className={activeSection === "#about" ? "is-active" : ""}
+          >
+            ABOUT
+          </button>
+          <button
+            onClick={() => handleButtonClick("#experience")}
+            data-section="#experience"
+            className={activeSection === "#experience" ? "is-active" : ""}
+          >
+            EXPERIENCE
+          </button>
+          <button
+            onClick={() => handleButtonClick("#contact")}
+            data-section="#contact"
+            className={activeSection === "#contact" ? "is-active" : ""}
+          >
+            CONTACT
+          </button>
+        </div>
+      </div>
+    </div>
+
+  {/* khalid  */}
+
+    <div className="card" data-state={activeSection}>
+    <div className="card-header">
+      <div
+        className="card-cover"
+        style={{
+          backgroundImage:
+            "url('https://play-lh.googleusercontent.com/tP1lwh_oLWW3XuX9qBKdKKNIBUNfQ_chTM_1GxuTxhnNb_fXYtMzB4gKBoVJx7RviSk')",
+        }}
+      ></div>
+      <img
+        className="card-avatar"
+        src="https://media.licdn.com/dms/image/D4E03AQFeh3JOjHO7eQ/profile-displayphoto-shrink_400_400/0/1689808542545?e=1696464000&v=beta&t=sa9cAbUKEHR7mcqLCRGLI6Lp6u86R2VD2rIqEDpv9vM"
+        alt="avatar"
+      />
+      <h1 className="card-fullname">Oyindamola J Akanbi</h1>
+      <h2 className="card-jobtitle">Software Engineer</h2>
+    </div>
+    <div className="card-main">
+      <div
+        className={`card-section ${
+          activeSection === "#about" ? "is-active" : ""
+        }`}
+        id="about"
+      >
+        <div className="card-content">
+          <div className="card-subtitle">ABOUT</div>
+          <p className="card-desc">
+            I am a Software Engineer with a passion for building scalable
+          </p>
+        </div>
+        <div className="card-social">
+          {/* Social media icons here */}
+        </div>
+      </div>
+      <div
+        className={`card-section ${
+          activeSection === "#experience" ? "is-active" : ""
+        }`}
+        id="experience"
+      >
+        {/* Experience section content here */}
+        <div className="card-content">
+          <div className="card-subtitle">WORK EXPERIENCE</div>
+          <div className="card-timeline">
+            <div className="card-item" data-year="2024">
+              <div className="card-item-title">
+              Returning Software Engineer Intern <span> @ Salesforce</span>
+              </div>
+              <div className="card-item-desc">
+               
+              </div>
+            </div>
+            <div className="card-item" data-year="2023">
+              <div className="card-item-title">
+                Future Tech Launchpad  <span> @ SalesForce</span>
+              </div>
+              <div className="card-item-desc">
+              Full Stack Web Development PERN stack!
+              </div>
+            </div>
+          
+           
+          </div>
+        </div>
+      </div>
+      <div
+        className={`card-section ${
+          activeSection === "#contact" ? "is-active" : ""
+        }`}
+        id="contact"
+      >
+        {/* Contact section content here */}
+        <div className="card-content">
+          <div className="card-subtitle">CONTACT</div>
+          <div className="card-contact-wrapper">
+            <div className="card-contact">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0118 0z" />
+                <circle cx="12" cy="10" r="3" />
+              </svg>
+              415 Mission St California City, CA 94105
+            </div>
+            <div className="card-contact">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path d="M12 19v-6M12 8c0-2.2 3.6-4 8-4s8 1.8 8 4-3.6 4-8 4-8-1.8-8-4z" />
+              </svg>
+              oakanbi13@gmail.com
+            </div>
+            <div className="card-contact">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path d="M22 2l-10 9L2 2" />
+                <path d="M22 2l-1 6 7 7-9 9 1 6 10-9 9 9 1-6-7-7 9-9z" />
+              </svg>
+              (267) 709-5092
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="card-buttons">
+        <button
+          onClick={() => handleButtonClick("#about")}
+          data-section="#about"
+          className={activeSection === "#about" ? "is-active" : ""}
+        >
+          ABOUT
+        </button>
+        <button
+          onClick={() => handleButtonClick("#experience")}
+          data-section="#experience"
+          className={activeSection === "#experience" ? "is-active" : ""}
+        >
+          EXPERIENCE
+        </button>
+        <button
+          onClick={() => handleButtonClick("#contact")}
+          data-section="#contact"
+          className={activeSection === "#contact" ? "is-active" : ""}
+        >
+          CONTACT
+        </button>
+      </div>
+    </div>
+  </div>
+
+
+  {/* hassani */}
+
+  <div className="card" data-state={activeSection}>
+  <div className="card-header">
+    <div
+      className="card-cover"
+      style={{
+        backgroundImage:
+          "url('https://play-lh.googleusercontent.com/tP1lwh_oLWW3XuX9qBKdKKNIBUNfQ_chTM_1GxuTxhnNb_fXYtMzB4gKBoVJx7RviSk')",
+      }}
+    ></div>
+    <img
+      className="card-avatar"
+      src="https://media.licdn.com/dms/image/D4E03AQFeh3JOjHO7eQ/profile-displayphoto-shrink_400_400/0/1689808542545?e=1696464000&v=beta&t=sa9cAbUKEHR7mcqLCRGLI6Lp6u86R2VD2rIqEDpv9vM"
+      alt="avatar"
+    />
+    <h1 className="card-fullname">Oyindamola J Akanbi</h1>
+    <h2 className="card-jobtitle">Software Engineer</h2>
+  </div>
+  <div className="card-main">
+    <div
+      className={`card-section ${
+        activeSection === "#about" ? "is-active" : ""
+      }`}
+      id="about"
+    >
+      <div className="card-content">
+        <div className="card-subtitle">ABOUT</div>
+        <p className="card-desc">
+          I am a Software Engineer with a passion for building scalable
+        </p>
+      </div>
+      <div className="card-social">
+        {/* Social media icons here */}
+      </div>
+    </div>
+    <div
+      className={`card-section ${
+        activeSection === "#experience" ? "is-active" : ""
+      }`}
+      id="experience"
+    >
+      {/* Experience section content here */}
+      <div className="card-content">
+        <div className="card-subtitle">WORK EXPERIENCE</div>
+        <div className="card-timeline">
+          <div className="card-item" data-year="2024">
+            <div className="card-item-title">
+            Returning Software Engineer Intern <span> @ Salesforce</span>
+            </div>
+            <div className="card-item-desc">
+             
+            </div>
+          </div>
+          <div className="card-item" data-year="2023">
+            <div className="card-item-title">
+              Future Tech Launchpad   <span> @ SalesForce</span>
+            </div>
+            <div className="card-item-desc">
+            Full Stack Web Development PERN stack!
+            </div>
+          </div>
+        
+         
+        </div>
+      </div>
+    </div>
+    <div
+      className={`card-section ${
+        activeSection === "#contact" ? "is-active" : ""
+      }`}
+      id="contact"
+    >
+      {/* Contact section content here */}
+      <div className="card-content">
+        <div className="card-subtitle">CONTACT</div>
+        <div className="card-contact-wrapper">
+          <div className="card-contact">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0118 0z" />
+              <circle cx="12" cy="10" r="3" />
+            </svg>
+            415 Mission St California City, CA 94105
+          </div>
+          <div className="card-contact">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M12 19v-6M12 8c0-2.2 3.6-4 8-4s8 1.8 8 4-3.6 4-8 4-8-1.8-8-4z" />
+            </svg>
+            oakanbi13@gmail.com
+          </div>
+          <div className="card-contact">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M22 2l-10 9L2 2" />
+              <path d="M22 2l-1 6 7 7-9 9 1 6 10-9 9 9 1-6-7-7 9-9z" />
+            </svg>
+            (267) 709-5092
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="card-buttons">
+      <button
+        onClick={() => handleButtonClick("#about")}
+        data-section="#about"
+        className={activeSection === "#about" ? "is-active" : ""}
+      >
+        ABOUT
+      </button>
+      <button
+        onClick={() => handleButtonClick("#experience")}
+        data-section="#experience"
+        className={activeSection === "#experience" ? "is-active" : ""}
+      >
+        EXPERIENCE
+      </button>
+      <button
+        onClick={() => handleButtonClick("#contact")}
+        data-section="#contact"
+        className={activeSection === "#contact" ? "is-active" : ""}
+      >
+        CONTACT
+      </button>
+    </div>
+  </div>
+</div>
+</div>
+
+</>
+
+
+  
+  );
+};
+
+export default Card;
